0432369425b7e12ca14e17060fcbec3066363a2b
Maxim Rikhm <55616314+Drogen371@users.noreply.github.com>
2020-01-23 16:27:48 +0300

    Delete lab2ReadTxt.lisp

--- lab2ReadTxt.lisp
+++
@@ -1,5 +0,0 @@
-(let ((in (open "lab2_requiredTxt.txt" :if-does-not-exist nil)))
-      (when in
-            (loop for line = (read-line in nil)
-            while line do (format t "~a~%" line))
-      (close in)))
